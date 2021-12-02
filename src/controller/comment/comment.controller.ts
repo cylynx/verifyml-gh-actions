5395ab43851b1569d33d818d53fa33cf1a7ce0ed
Yinghua Chai <yinghuachai0@gmail.com>
2021-12-02 13:19:16 +0800

    feat: make comment with octokit

--- src/controller/comment/comment.controller.ts
+++ src/controller/comment/comment.controller.ts
@@ -1,26 +1,28 @@
 import { TestResult } from '../protobuf';
 import type { CommentInterface } from './comment.interface';
+import * as github from '@actions/github';
+import * as core from '@actions/core';
 
 class CommentController implements CommentInterface {
-  public constructMarkdown(result: TestResult, path: string): string {
-    const description = this.constructDescription(path);
+  private constructMarkdown(result: TestResult, path: string): string {
+    const title = this.constructTitle(path);
     const testSummary = this.constructResult(result);
 
     const mdTemplate = `
-    # 🔖 VerifyML Report
-
-    ${description}
-
-    ## 🔍 Test Result Summary
-
+    ${title}
     ${testSummary}
     `;
 
     return mdTemplate;
   }
 
-  private constructDescription(path: string) {
-    return `Your test result for dataset in <code>${path}</code> is ready!`;
+  private constructTitle(path: string) {
+    return `
+    # 🔖 VerifyML Report
+
+    Your test result for dataset in <code>${path}</code> is ready!
+
+    `;
   }
 
   private constructResult(result: TestResult) {
@@ -31,14 +33,36 @@ class CommentController implements CommentInterface {
     } = result;
 
     const table = `
+     ## 🔍 Test Result Summary
+
     |Test Type|Passed|Failed|
     |---------|:---:|:-----:|
-    |Explainability Analysis| ${EA.passCount}| ${EA.failCount} |
-    |Quantitative Analysis| ${QA.passCount}| ${QA.failCount} |
-    |Fairness Analysis| ${FA.passCount}| ${FA.failCount} |
+    |Explainability Analysis| ${EA.passCount} | ${EA.failCount} |
+    |Quantitative Analysis| ${QA.passCount} | ${QA.failCount} |
+    |Fairness Analysis| ${FA.passCount} | ${FA.failCount} |
+
     `;
 
     return table;
+  }
+
+  public makeComment(result: TestResult, path: string) {
+    const comment = this.constructMarkdown(result, path);
+    const githubToken = core.getInput('GITHUB_TOKEN');
+
+    const context = github.context;
+    if (context.payload.pull_request == null) {
+      throw new Error('no pull request found.');
+    }
+
+    const pullRequestNumber = context.payload['pull_request_number'];
+
+    const octokit = github.getOctokit(githubToken);
+    octokit.rest.issues.createComment({
+      ...context.repo,
+      issue_number: pullRequestNumber,
+      body: comment,
+    });
   }
 }
 
--- src/main.ts
+++ src/main.ts
@@ -9,27 +9,21 @@ const main = async () => {
   const commentController = new CommentController();
 
   try {
-    // const cwd = core.getInput('cwd');
-    // const path = core.getInput('data-path');
-    // if (!path) {
-    //   core.setFailed(
-    //     '`data-path` is not provided, the path to dataset is required for us to compute the results.',
-    //   );
-    //   throw new Error('Missing `data-path` parameters');
-    // }
+    const cwd = core.getInput('cwd');
+    const path = core.getInput('data-path');
+    if (!path) {
+      core.setFailed(
+        '`data-path` is not provided, the path to dataset is required for us to compute the results.',
+      );
+      throw new Error('Missing `data-path` parameters');
+    }
 
-    // const filePath = `${cwd}${path}`;
-    const filePath =
-      '/Users/yinghua/Projects/cylynx/verifyml-report/public/data/credit-card-fraud.proto';
+    const filePath = `${cwd}${path}`;
+
     const data = await readerController.readDataFromPath(filePath);
     const modelCard = protobufController.decodeMessage(data);
     const testResult = protobufController.deriveTestResults(modelCard);
-    const commentContent = commentController.constructMarkdown(
-      testResult,
-      filePath,
-    );
-
-    console.log(commentContent);
+    commentController.makeComment(testResult, filePath);
   } catch (error: any) {
     core.setFailed(error.message);
   }
